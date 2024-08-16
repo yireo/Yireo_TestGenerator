9fbdeb245774c4cf54c8f41f521a5fd7232a3497
Jisse Reitsma <jisse@yireo.com>
2024-08-16 13:02:12 +0200

    Skip instantiation unit test with complex classes

--- Generator/UnitTest/GenericTestGenerator.php
+++ Generator/UnitTest/GenericTestGenerator.php
@@ -2,6 +2,11 @@
 
 namespace Yireo\TestGenerator\Generator\UnitTest;
 
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Model\AbstractModel;
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb as AbstractDbCollection;
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 use ReflectionClass;
 use Yireo\TestGenerator\Generator\PhpGenerator;
 use Yireo\TestGenerator\Generator\PhpGeneratorFactory;
@@ -44,12 +49,17 @@ class GenericTestGenerator
         return <<<EOF
 \${$variableName} = \$this->getMockBuilder({$className}::class)->disableOriginalConstructor()->getMock();
 \$this->assertInstanceOf({$className}::class, \${$variableName});
+
 // @todo: Extend upon this test
 EOF;
     }
 
     private function getTestInstantiationWithMocks(ClassStub $classStub): string
     {
+        if ($this->skipInstantationWithMocks($classStub->getFullQualifiedClassName())) {
+            return "\$this->markTestSkipped('Test skipped because constructor is too complex for mocking');";
+        }
+
         $className = $classStub->getClassName();
         $variableName = lcfirst($className);
         $reflectionClass = new ReflectionClass($classStub->getFullQualifiedClassName());
@@ -61,18 +71,24 @@ EOF;
             foreach ($reflectionClass->getConstructor()->getParameters() as $reflectionParameter) {
                 $parameterName = $reflectionParameter->getName();
                 $parameterType = $reflectionParameter->getType();
+
+
                 if (null === $parameterType) {
                     continue;
                 }
 
-                if ($reflectionParameter->isDefaultValueAvailable()) {
+                $parameterClass = '\\'.$parameterType->getName();
+                $isClass = interface_exists($parameterClass) || class_exists($parameterClass);
+
+                if (false === $isClass && $reflectionParameter->isDefaultValueAvailable()) {
                     continue;
                 }
 
-                $parameterClass = '\\'.$parameterType->getName();
-                if (interface_exists($parameterClass) || class_exists($parameterClass)) {
+                if ($isClass) {
                     $parameterClassName = basename(str_replace('\\', '/', $parameterClass));
-                    $test .= "\$$parameterName = \$this->createMock({$parameterClassName}::class);\n\n";
+                    $test .= "\$$parameterName = \$this->createMock({$parameterClassName}::class);\n";
+                    $test .= "//\${$parameterName}->method('getFoo')->willReturn('bar');\n";
+                    $test .= "\n";
                     $this->phpGenerator->addUse($parameterClass);
                 }
 
@@ -85,9 +101,29 @@ EOF;
         $test .= <<<EOF
 \${$variableName} = new {$className}({$constructorArguments});
 \$this->assertInstanceOf({$className}::class, \${$variableName});
+
 // @todo: Extend upon this test and guarantee that instantiation works
 EOF;
 
         return $test;
+    }
+
+    private function skipInstantationWithMocks(string $className): bool
+    {
+        foreach ($this->getSkipClasses() as $skipClass) {
+            if ($className === $skipClass || is_subclass_of($className, $skipClass)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function getSkipClasses(): array
+    {
+        return [
+            AbstractCollection::class,
+            AbstractModel::class,
+        ];
     }
 }
